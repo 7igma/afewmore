520eed762b90329b1d2876f3879a89d23e53b214
7igma <snunes012@gmail.com>
2017-04-17 15:25:13 -0400

    finalizing

--- afewmore.c
+++ afewmore.c
@@ -100,7 +100,8 @@ int contains(char* str, char** arr)
 int main(int argc, char** argv)
 {
 	int i;
-	char* dir = NULL; //the directory to copy from
+	int verbose = 0;
+	char dir[200]; //the directory to copy from
 	int count = 1; //FOR THE SAKE OF TESTING
 									//the number of instances to create
 									//since the default is 10, I think it should initially be set to 10
@@ -113,12 +114,23 @@ int main(int argc, char** argv)
 	char* ip= malloc(sizeof(char) * 100); //dns of the most recently created instance
 	char* curr_dns= malloc(sizeof(char) * 100);
 
+
+	char addresses[50][100];
+
 	char* token;
 	const char s[2] = ".";
 
 	FILE* fp;
 	char path[1035];
 	//char* output= malloc(sizeof(char) * 1035);
+
+	/*
+	char ipaddr[1024];
+	fp = popen("curl ipinfo.io/ip", "r");
+	fgets(ipaddr, sizeof(ipaddr)-1, fp);
+	printf("IP addr: %s\n", ipaddr);
+	pclose(fp);
+	*/
 
 	char command1[200];
 	char command2[200];
@@ -133,12 +145,19 @@ int main(int argc, char** argv)
 	{
 		printf("Provide an EC2 instance id as an argument to duplicate it.\n");
 		printf("Options:\n");
-		printf("-d dir: copy the contents in this directory from the source instance to the new instances\n");
+		printf("-d dir: copy the contents in this directory from the source instance to the new instances (default is ./data)\n");
 		printf("-n num: specify the number of new instances to create (default = 10) \n");
 		printf("-v: provide verbose output\n");
 		return 0;
 	}
+	if ((i = contains("-v", argv)) != -1)
+	{
+		verbose = 1;
+	}
+
 	inst_id = argv[1];
+	sprintf(dir, "data");
+
 	if ((i = contains("-d", argv)) != -1)
 	{
 		if (argv[i+1] == NULL)
@@ -146,7 +165,8 @@ int main(int argc, char** argv)
 			printf("No directory specified after -d\n");
 			return 0;
 		}
-		dir = argv[i+1];
+		sprintf(dir, "%s", argv[i+1]);
+		//dir = argv[i+1];
 	}
 	if ((i = contains("-n", argv)) != -1)
 	{
@@ -155,7 +175,6 @@ int main(int argc, char** argv)
 			printf("No value specified after -n\n");
 			return 0;
 		}
-		/* TODO: check that the given argument is in fact a number value */
 		/* currently assumes that the user will provide a number after -n */
 		count = atoi(argv[i+1]);
 	}
@@ -165,21 +184,15 @@ int main(int argc, char** argv)
 	{
 		system("aws ec2 create-key-pair --key-name key --query 'KeyMaterial' --output text > key.pem");
 		system("aws ec2 create-security-group --group-name sg --description \"default\"");
-		//system("chmod 400 key.pem")
+		system("chmod 400 key.pem");
 	}
 	
-	//strcpy(command1, "aws ec2 run-instances --image-id am");
-	//strcat(command1, inst_id);
-	//strcat(command1, " --security-groups sg --count 1 --instance-type t1.micro --key-name key --query \'Instances[0].InstanceId\'");
-	//sprintf(command1, "aws ec2 run-instances --image-id am");
-	//sprintf(command1 + strlen(command1), inst_id);
-	//sprintf(command1 + strlen(command1), " --security-groups sg --count 1 --instance-type t1.micro --key-name key --query \'Instances[0].InstanceId\'");
 	sprintf(command1, "aws ec2 run-instances --image-id am%s --security-groups sg --count 1 --instance-type t1.micro --key-name key --query \'Instances[0].InstanceId\'", inst_id);
 	//printf("%s\n", command1);
 	
 	for(i=0; i< count; i++)
 	{
-		printf("%s\n", command1);
+		if (verbose) {printf("%s\n", command1);}
 		fp = popen(command1, "r");
 		if (fp == NULL)
 		{
@@ -205,7 +218,9 @@ int main(int argc, char** argv)
 			//strcat(all_instances, " ");
 			strcat(all_instances, curr_instance);
 		}
-		printf("%s", all_instances);
+		if (verbose){
+			printf("All instance ids: %s", all_instances);
+		}
 		//strcpy(curr_instance, output);
 		//strcpy(output, "");
 		//printf("%s", curr_instance);
@@ -216,7 +231,7 @@ int main(int argc, char** argv)
 		curr_instance[strlen(curr_instance) - 1] = '\0';
 
 		sprintf(command2, "aws ec2 describe-instances --instance-ids %s --query \'Reservations[0].Instances[0].PublicIpAddress\'", curr_instance);
-		printf("%s\n", command2);
+		if (verbose) {printf("%s\n", command2);}
 		fp = popen(command2, "r");
 		if (fp == NULL)
 		{
@@ -232,7 +247,7 @@ int main(int argc, char** argv)
 			strcpy(ip, path);
 			//printf("%s", output);
 		}
-		printf("%s", ip);
+		if (verbose) {printf("%s", ip);}
 		pclose(fp);
 
 		path[0]='\0';
@@ -248,31 +263,32 @@ int main(int argc, char** argv)
 			token = strtok(NULL, s);
 		}
 		strcat(curr_dns, ".compute-1.amazonaws.com");
-		printf("%s\n", curr_dns);
+		if(verbose){printf("%s\n", curr_dns);}
 		if(i==0)
 		{
 			strcpy(all_dns, curr_dns);
+			strcpy(addresses[i], curr_dns);
 		}
 		else
 		{
 			strcat(all_dns, " ");
 			strcat(all_dns, curr_dns);
+			strcpy(addresses[i], curr_dns);
 		}
 	}
-	printf("%s\n", all_dns);
+	if(verbose){printf("%s\n", all_dns);}
 
 	insts = my_str2vect(all_instances);
 	dnss = my_str2vect(all_dns);
 
-	printf("Waiting for instances to start up...\n");
+	if(verbose){printf("Waiting for instances to start up...\n");}
 
-	sleep(120);
+	sleep(30);
 
 	for(i=0; i<count; i++)
 	{
-		printf("%s\n", dnss[i]);
-		//HARDCODED: key(and key directory), dir to send(and dir directory)
-		sprintf(command3, "scp -i key.pem -r ../data ubuntu@%s:", dnss[i]);
+		sprintf(command3, "scp -i key.pem -r %s ubuntu@%s", dir, addresses[i]);
+		if (verbose) {printf("Running command: %s\n", command3);}
 		fp = popen(command3, "w");
 		if (fp == NULL)
 		{
@@ -286,7 +302,8 @@ int main(int argc, char** argv)
 		pclose(fp);
 	}
 
-	printf("%s", all_instances);
+	//printf("%s", all_instances);
+	if (verbose) {printf("Transfer complete.\n");}
 	
 	return 0;
 }
